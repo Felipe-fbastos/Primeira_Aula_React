5be5296e3282b2069dbefe2c1b7c96360e502971
felipe-fbastos <felipeferreirabastos2@gmail.com>
2025-02-13 22:15:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p> Bolacha sabor agridoce </p>
+      <p> Bolacha ou Biscoito? </p>
+      <p> coito? </p>
+      <ol>
+        <li>Acordar sete horas</li>
+        <li>Tomar café</li>
+        <li>P de programar o resto do dia</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
